cbd2f3f4356d3a6de0ecbe73f7fda70e7af1bfe2
Anaclara Ferrando <anaclaraferrando95@gmail.com>
2025-02-12 17:34:07 -0300

    seccion customer

--- src/Home.java
+++ src/Home.java
@@ -120,7 +120,7 @@ public class Home extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        // TODO add your handling code here:
+        new ManageCustomer().setVisible(true);
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
